88115d10c0d961840361b98710542b8b5fae759b
niyamk <niyam4103@gmail.com>
2025-10-28 17:48:50 +0530

    changed theme to dark

---
+++ Migrations/20251028120317_RemoveCardColor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PublicNotes.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCardColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CardColor",
+                table: "Notes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CardColor",
+                table: "Notes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
